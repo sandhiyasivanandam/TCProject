ed9268e5f49ac7de8b2c8c0e4876e597787ec746
sandhiya <sandhiya.sivanandam@gmail.com>
2022-05-09 12:21:09 +0530

    First Commit

---
+++ TestProject12/Script/Unit1.js
@@ -0,0 +1,17 @@
+﻿function Test1(User)
+{
+  //Opens the specified URL in a running instance of the specified browser.
+  Browsers.Item(btChrome).Navigate("http://secure.smartbearsoftware.com/samples/TestComplete11/WebOrders/Login.aspx");
+  //Sets the specified position and size for the specific BrowserWindow object.
+  Aliases.browser.BrowserWindow.Position(0, 0, 1920, 1030);
+  //Waits until the browser loads the page and is ready to accept user input.
+  Aliases.browser.pageWebOrdersLogin.Wait();
+  //Sets the text in the 'textboxUsername' text editor.
+  Aliases.browser.pageWebOrdersLogin.formAspnetform.textboxUsername.SetText(User);
+  //Enters '[Tab]' in the 'textboxUsername' object.
+  Aliases.browser.pageWebOrdersLogin.formAspnetform.textboxUsername.Keys("[Tab]");
+  //Sets the text Project.Variables.Password1 in the 'passwordboxPassword' text editor.
+  Aliases.browser.pageWebOrdersLogin.formAspnetform.passwordboxPassword.SetText(Project.Variables.Password1);
+  //Clicks the 'submitbuttonLogin' button.
+  Aliases.browser.pageWebOrdersLogin.formAspnetform.submitbuttonLogin.ClickButton();
+}
---
+++ TestProject12/Script/Unit2.js
@@ -0,0 +1,7 @@
+﻿function test(){
+Browsers.Item(btChrome).Navigate("http://secure.smartbearsoftware.com/samples/TestComplete11/WebOrders/Login.aspx");
+
+Aliases.browser.pageWebOrdersLogin.formAspnetform.textboxUsername.SetText("Tester");
+Aliases.browser.pageWebOrdersLogin.formAspnetform.passwordboxPassword.SetText("test");
+Aliases.browser.pageWebOrdersLogin.formAspnetform.submitbuttonLogin.Click();
+}
